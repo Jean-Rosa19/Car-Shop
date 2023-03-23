b2f489a856bcf4d872342a3a6a6fb56748fc26f3
jean-rosa <jeancarlosrosa22@gmail.com>
2023-03-23 09:48:04 -0300

    refatorando interface iCar

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,35 +1,35 @@
 import ICar from '../Interfaces/ICar';
 
 class Car {
-  protected id?: string | undefined;
+  id?: string | undefined;
   protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean;
-  protected buyValue: number;
+  year: number;
+  color: string;
+  status: boolean;
+  buyValue: number;
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(
     {
+      id,
       model,
       year,
       color,
+      status,
       buyValue,
       doorsQty,
-      seatsQty,
-      status,
-      id, 
+      seatsQty, 
     } : ICar,
   ) {
+    this.id = id;
     this.model = model;
     this.year = year;
     this.color = color;
+    this.status = status || false;
     this.buyValue = buyValue;
     this.doorsQty = doorsQty;
     this.seatsQty = seatsQty;
-    this.id = id;
-    this.status = status || false;
   }
 
   // public setId(id: string) {
